03cd92b00c5943f104ba8b787747056d93987df6
Joel Tay <joeltwh1999@gmail.com>
2024-03-25 12:36:14 +0800

    init POC

---
+++ emoteam-auth/app.py
@@ -0,0 +1,116 @@
+from flask import Flask, redirect, session, request, jsonify
+from flask_cors import CORS
+import spotipy
+import os
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+load_dotenv()
+CORS(app)
+app.secret_key = os.urandom(24)
+
+authorization_endpoint = "https://accounts.spotify.com/authorize"
+token_endpoint = "https://accounts.spotify.com/api/token"
+redirect_uri = f"{os.environ.get('API_URL')}/callback"
+scope = 'user-read-recently-played'
+
+spotify_client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+spotify_client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
+app_url = os.environ.get('APP_URL')
+
+
+@app.route('/check-token')
+def check_token():
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Bearer '):
+        access_token = auth_header.split(' ')[1]
+        # Create a temporary Spotify client with this token
+        sp_temp = spotipy.Spotify(auth=access_token)
+        try:
+            # Attempt a simple request to check if the token is valid
+            sp_temp.current_user()
+            # If this request succeeds, the token is valid
+            return jsonify(valid=True)
+        except spotipy.SpotifyException as e:
+            # If there's an exception, assume the token is invalid or expired
+            return jsonify(valid=False, error=str(e)), 401
+    else:
+        return jsonify(valid=False, error="Authorization header missing"), 400
+
+
+@app.route('/')
+def verify():
+    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=spotify_client_id,
+                                           client_secret=spotify_client_secret,
+                                           redirect_uri=redirect_uri,
+                                           scope=scope)
+    auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
+    return redirect(auth_url)
+
+
+@app.route('/callback')
+def callback():
+    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=spotify_client_id,
+                                           client_secret=spotify_client_secret,
+                                           redirect_uri=redirect_uri,
+                                           scope=scope)
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+
+    # Saving the access token along with all other token related info
+    session["token_info"] = token_info
+
+    return redirect(f'{app_url}?code={token_info["access_token"]}')
+
+
+@app.route('/get-recent', methods=['POST'])
+def get_recent():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        token = auth_header.split(" ")[1]
+    else:
+        return jsonify({"error": "Authorization header is missing"}), 401
+
+    sp = spotipy.Spotify(auth=token)
+    data = request.get_json()
+    try:
+        return jsonify(sp.current_user_recently_played(limit=data.get('limit', 20), after=data.get('after')))
+    except spotipy.SpotifyException as e:
+        return jsonify({"error": str(e)}), 400
+
+
+# def get_token(sess):
+#     token_valid = False
+#     token_info = sess.get("token_info", {})
+#
+#     # Checking if the session already has a token stored
+#     if not (sess.get('token_info', False)):
+#         token_valid = False
+#         return token_info, token_valid
+#
+#     # Checking if token has expired
+#     now = int(time.time())
+#     is_token_expired = sess.get('token_info').get('expires_at') - now < 60
+#
+#     # Refreshing token if it has expired
+#     if is_token_expired:
+#         sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=spotify_client_id,
+#                                                client_secret=spotify_client_secret,
+#                                                redirect_uri=redirect_uri,
+#                                                scope=scope)
+#         token_info = sp_oauth.refresh_access_token(sess.get('token_info').get('refresh_token'))
+#
+#     token_valid = True
+#     return token_info, token_valid
+
+
+# TODO: make sure the correct app.run is used
+if __name__ == '__main__':
+    # DEV
+    app.run(host='0.0.0.0', port=3000)
+
+    # PROD
+    # app.run()
---
+++ emoteam/app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import requests
+import datetime
+import calendar
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_url = os.environ.get('API_URL')
+
+st.title("Emoteam")
+
+if 'access_token' not in st.session_state:
+    st.session_state.access_token = None
+
+
+def validate_token(token):
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(f'{api_url}/check-token', headers=headers)
+    return response.json()
+
+
+def check_login():
+    params = st.query_params
+    if params.get('code') is not None:
+        # redirected from callback
+        st.session_state.access_token = params.get('code')
+        st.query_params.clear()
+
+    if st.session_state.access_token is not None:
+        resp = validate_token(st.session_state.access_token)
+        if resp.get('valid'):
+            return st.session_state.access_token
+        else:
+            # lazily, log in again, instead of refreshing
+            st.write(f"""<meta http-equiv="refresh" content="0; url='{api_url}'">""", unsafe_allow_html=True)
+            st.stop()
+    else:
+        st.write(f"""<meta http-equiv="refresh" content="0; url='{api_url}'">""", unsafe_allow_html=True)
+        st.stop()
+
+
+def get_preview_url(item):
+    track = item['track']
+    if track is not None:
+        return track['preview_url']
+    return None
+
+
+access_token = check_login()
+limit = st.slider("Maximum number of songs:", 1, 50, 20, 1)
+today = datetime.datetime.now()
+after_date = st.date_input(
+    "Retrieve songs from:",
+    max_value=today,
+    value=today - datetime.timedelta(days=7),
+    format='DD/MM/YYYY'
+)
+proc_req = st.button('Get songs')
+if proc_req:
+    headers = {'Authorization': f'Bearer {access_token}'}
+    timestamp = calendar.timegm(after_date.timetuple())  # seconds
+    body = {'limit': limit, 'after': timestamp * 1000}
+    songs = requests.post(f'{api_url}/get-recent', headers=headers, json=body).json()
+    st.write(songs)
+    tracks = list(map(get_preview_url, songs['items']))
+    tracks = list(filter(lambda url: url is not None, tracks))
+    st.write(tracks)
